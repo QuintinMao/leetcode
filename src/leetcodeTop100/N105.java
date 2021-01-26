8e00a10f359a4a989b6b050e1832e06d030d584a
QuintinMao <i@mao.gs>
2021-01-26 11:52:34 +0800

    add warnings

--- src/leetcodeTop100/N105.java
+++ src/leetcodeTop100/N105.java
@@ -15,6 +15,7 @@ public class N105 {
 	public static void main(String[] args) {
 		int[] preorder = { 3, 9, 20, 15, 7 };
 		int[] inorder = { 9, 3, 15, 20, 7 };
+		@SuppressWarnings("unused")
 		TreeNode res = new N105().buildTree(preorder, inorder);
 
 	}
